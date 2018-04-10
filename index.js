16e0be246fdb7852d6cb8d6494ad5c992700982f
Maksym Kotielnikov <catavary@gmail.com>
2018-04-10 21:34:11 +0200

    bittrex

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hello2')
+import bittrex from 'node-bittrex-api'
+import util from 'util'
+
+
+
+bittrex.getorderbook({ market : 'BTC-XVG', type : 'both' }, function( data, err ) {
+  console.log(util.inspect( data, false, null ))
+});
+
